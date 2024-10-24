03d82f2ef2919c5595be78261940dd105ae2d70b
Kreida Anton <kreida.anton@yandex.ru>
2024-10-24 14:26:10 +0500

    main | install graphql, custom admin app.tsx

---
+++ src/admin/app.tsx
@@ -0,0 +1,24 @@
+import type { StrapiApp } from '@strapi/strapi/admin';
+
+import logo from "./extensions/favicon.png";
+
+const config = {
+  locales: ["en", "ru"],
+  auth: {
+    logo,
+  },
+  notifications: false,
+  menu: {
+    logo,
+  },
+  tutorials: false,
+}
+
+const bootstrap = (app: StrapiApp) => {
+  console.log(app);
+}
+
+export default {
+  config,
+  bootstrap,
+};
