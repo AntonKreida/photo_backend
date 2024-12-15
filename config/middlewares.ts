55b186c817ae423c722e02aa8fbd57342fcac913
Kreida Anton <kreida.anton@yandex.ru>
2024-12-15 19:08:26 +0500

    main | fix

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,15 +2,7 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://5.188.114.167', 'http://5.188.114.167:1337'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      keepHeaderOnError: true,
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
